36f62642853c403c62a84059a35d30ad6b856fdc
coolight543 <robamiao@gmail.com>
2025-11-04 22:40:18 +0000

    k

--- Patterns.java
+++ Patterns.java
@@ -1,1 +1,33 @@
+// Kuang Miao
+//11 4 2025
+// This project is meant to print out the assigned patterns
+public class Patterns{
 
+    public static void stars (int rows)
+    {
+        String stars = "*";
+        int row = 0;
+        while (row<rows)
+        {
+            row++;
+            System.out.println(stars);
+            stars += "**";
+          }
+    }
+    public static void triangle (int rows)
+    {
+        int numbers = 1;
+        int row = 0; 
+        int i = 0;
+        while (row<rows)
+        {
+            row++; 
+            System.out.println(numbers);
+            numbers++;
+        }
+    }
+    public static void main (String [] args){
+    stars(8);
+    triangle(5);
+    }
+}
