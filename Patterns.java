c9f7052d1c8db41b415d28e903af6a0e4a02d75f
coolight543 <robamiao@gmail.com>
2025-11-06 01:20:35 +0000

    ijiji

--- Patterns.java
+++ Patterns.java
@@ -16,30 +16,82 @@ public class Patterns{
     }
     public static void triangle (int rows)
     {
-        int row = 0; 
-        while (row<rows)
+        int i = 0; 
+        while (i<rows)
         {
-            row++;
+            i++;
             int numbers = 0;
-            while (numbers<row)
+            while (numbers<i)
             {
-                System.out.print(row);
+                System.out.print(i);
                 numbers++;
             }
             System.out.println();
         }
     }
+    public static void odds (int start)
+    {
+            for (int number = start; number>0; number -=2)
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    System.out.print(number);
+                }
+                System.out.println();
+            }
+        
+    }
+    
     public static void EO (int maxE)
     {
         String letter = "E";
-        if (maxE % 2 == 0)
+        for (int i = 1; i <= maxE; i+=2)
         {
-            letter = "O";
+            if (maxE % 2 == 0)
+            {
+                letter = "O";
+            }
+            for (int j = 0; j < i; j++)
+            {
+                System.out.print(letter);
+            }
+            System.out.println();
+        }
+        for (int i = 2; i <=maxE; i +=2)
+        {
+            if (maxE % 2 ==0)
+            {
+                letter = "E";
+            }
+            for (int j = 0; j<i; j++)
+            {
+                System.out.print(letter);
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void pyramid(int rows)
+    {
+        int length = rows * 2 - 1;
+        for (int i = 1; i<=rows; i++)
+        {
+            for (int j = 0; j < length; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+            length -=2;
         }
     }
     public static void main (String [] args){
     stars(8);
     triangle(5);
+    odds(9);
+    EO(6);
+    pyramid(5);
     }
 }
+// outer loop, how many rows there are
+// inner loop, how many letters/numbers I should print on each row
 
